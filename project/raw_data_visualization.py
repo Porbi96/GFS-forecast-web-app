4ed1e36232bdc2751392a05345ca264ce52e8bac
Porbi96 <spikasny@gmail.com>
2020-08-18 13:54:07 +0200

    test commit 10

--- project/raw_data_visualization.py
+++ project/raw_data_visualization.py
@@ -2,10 +2,9 @@ import os
 import re
 
 os.environ["PROJ_LIB"] = "C:\\Python\\Anaconda\\Library\\share"
-import requests, csv
+import requests, gdal, csv
 import matplotlib.pyplot as plt
 import numpy as np
-from osgeo import gdal
 from mpl_toolkits.basemap import Basemap
 from scipy.ndimage import convolve
 from typing import List
